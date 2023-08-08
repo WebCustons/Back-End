c1ec3acbc4d3f0ddd1c873be3fd928326a17d949
Brunohgs21 <brunohgs21@outlook.com>
2023-08-08 19:49:26 -0300

    feat: typing all adverts services return

--- src/controllers/adverts/createAdverts.controller.ts
+++ src/controllers/adverts/createAdverts.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { TAdvertRequest } from "../../interfaces/advert.interfaces";
+import { TAdvert, TAdvertRequest } from "../../interfaces/advert.interfaces";
 import { createAdvertService } from "../../services/adverts/createAdvert.service";
 import { Adverts } from "../../entities/adverts.entities";
 
 export const createAdvertsController = async (
   req: Request,
   res: Response
-): Promise<Response<Adverts>> => {
+): Promise<Response<TAdvert>> => {
   const advert: TAdvertRequest = req.body;
   const userId = req.params.id;
 
