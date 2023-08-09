b2eab5455ca74203b12607f3a7b72399ca29a731
natabgaf <natanfernandes52@gmail.com>
2023-08-09 16:40:45 -0300

    merge develop

--- src/controllers/adverts/updateAdverts.controller.ts
+++ src/controllers/adverts/updateAdverts.controller.ts
@@ -1,20 +1,20 @@
-import { Request, Response } from "express";
-import { updateAdvertService } from "../../services/adverts/updateAdvert.service";
-import {
-  TAdvertRequestUpdate,
-  TAdvertResponse,
-} from "../../interfaces/advert.interfaces";
+// import { Request, Response } from "express";
+// import { updateAdvertService } from "../../services/adverts/updateAdvert.service";
+// import {
+//   TAdvertRequestUpdate,
+//   TAdvertResponse,
+// } from "../../interfaces/advert.interfaces";
 
-export const updateAdvertsController = async (
-  req: Request,
-  res: Response
-): Promise<Response<TAdvertResponse>> => {
-  const advert: TAdvertRequestUpdate = req.body;
+// export const updateAdvertsController = async (
+//   req: Request,
+//   res: Response
+// ): Promise<Response<TAdvertResponse>> => {
+//   const advert: TAdvertRequestUpdate = req.body;
 
-  const updateAdverts: TAdvertResponse = await updateAdvertService(
-    Number(req.params.id),
-    advert
-  );
+//   const updateAdverts: TAdvertResponse = await updateAdvertService(
+//     Number(req.params.id),
+//     advert
+//   );
 
-  return res.json(updateAdverts);
-};
+//   return res.json(updateAdverts);
+// };
