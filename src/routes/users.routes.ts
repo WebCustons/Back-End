5f4093b006bedecf74861172cd368d6ce327b312
Christian <christianryuji@hotmail.com>
2023-08-09 16:16:32 -0300

    feature: login

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
-import { createUsersController } from "../controllers/User/createUsers.controller";
-import { schemaValidator } from "../middlewares/schema.middlewares";
-import { userSchemaRequest } from "../schemas/user.schema";
+import { createUsersController } from "../controllers/user/createUsers.controller"
+import { schemaValidator } from "../middlewares/schema.middlewares"
+import { userSchemaRequest } from "../schemas/user.schema"
 
 export const userRoutes = Router()
 
