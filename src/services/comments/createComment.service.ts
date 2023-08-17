85749ab35d11d3ed371d773fbb58ed035bc08edb
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-08-17 20:45:15 -0300

    feat:crud comments

--- src/services/comments/createComment.service.ts
+++ src/services/comments/createComment.service.ts
@@ -11,7 +11,7 @@ export const createCommentsService = async(userId:number,advertId:number,comment
         user:{id:Number(userId)},
         advert:{id:advertId}
     });
-
+    
     await commentRepository.save(newComments);
 
     const {id,comment,created_at,} = newComments;
---
+++ src/services/comments/readAllCommentAdvert.service.ts
@@ -0,0 +1,18 @@
+import {Adverts} from '../../entities/adverts.entities';
+import {AppDataSource} from '../../data-source';
+
+export const readAllCommentAdvertService = async(idAdvert:number)=>{
+
+    const repAdvert = AppDataSource.getRepository(Adverts);
+
+    const advert = await repAdvert.find({
+        where:{
+            id:idAdvert
+        },
+        relations:{
+            comments:true
+        }
+        
+    })
+    return advert[0].comments
+}
