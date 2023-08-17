34d1fbe89367175f4e2200fe25605b9a7f3b938e
natabgaf <natanfernandes52@gmail.com>
2023-08-17 09:45:40 -0300

    ipdate organization

--- src/controllers/login/login.controller.ts
+++ src/controllers/login/login.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import loginService from "../../services/login/login.service"
+import { loginService } from "../../services/login/login.service"
 
 export const loginController = async (
   req: Request,
