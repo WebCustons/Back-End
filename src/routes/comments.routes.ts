0dd07420de1e532133306716c42288d68168cf26
Gabriel Góes <matt_cariusp@hotmail.com>
2023-08-23 16:22:53 -0300

    feat: routeProtections admin cant update(user,adverts or images)

--- src/routes/comments.routes.ts
+++ src/routes/comments.routes.ts
@@ -1,10 +1,10 @@
 import { Router} from "express";
-import { notIsAdmin, verifyAuthToken } from "../middlewares/authorization.middleware";
+import { adminCantUseRoute,verifyAuthToken } from "../middlewares/authorization.middleware";
 import { advertsExistsbyId} from "../middlewares/adverts.middlewares";
 import { createCommentController } from "../controllers/comments/createComment.controller";
 import { readAllCommentAdvertController } from "../controllers/comments/readAllCommentAdvert.controller";
 import { deleteCommentController } from "../controllers/comments/deleteComment.controller";
-import { commentExistsbyId, isOwnerOrAdminComments} from "../middlewares/comments.middlewares";
+import { commentExistsbyId, isOwnerComments} from "../middlewares/comments.middlewares";
 import { updateCommentController } from "../controllers/comments/updateComment.controller";
 import { schemaValidator } from "../middlewares/schema.middlewares";
 import { commentSchemaRequest } from "../schemas/comment.schema";
@@ -15,10 +15,10 @@ commentsRoutes.post(
   "/advert/:id/",
   verifyAuthToken,
   advertsExistsbyId,
-  notIsAdmin,
+  adminCantUseRoute,
   schemaValidator(commentSchemaRequest),
   createCommentController
 );
 commentsRoutes.get("/advert/:id",verifyAuthToken,advertsExistsbyId,readAllCommentAdvertController);
-commentsRoutes.patch("/:id",verifyAuthToken,commentExistsbyId,isOwnerOrAdminComments,schemaValidator(commentSchemaRequest),updateCommentController);
-commentsRoutes.delete("/:id", verifyAuthToken,commentExistsbyId,isOwnerOrAdminComments,deleteCommentController);
+commentsRoutes.patch("/:id",verifyAuthToken,adminCantUseRoute,commentExistsbyId,isOwnerComments,schemaValidator(commentSchemaRequest),updateCommentController);
+commentsRoutes.delete("/:id", verifyAuthToken,commentExistsbyId,isOwnerComments,deleteCommentController);
